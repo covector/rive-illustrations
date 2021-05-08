1123c154cd7ff36fecae020b09f88e6ff078f37f
covector <60296643+covector@users.noreply.github.com>
2021-05-08 21:42:43 +0800

    canvas animation updates according to mouse move

--- feed-forward/script.ts
+++ feed-forward/script.ts
@@ -1,6 +1,36 @@
 import * as rive from "rive-js"
 
-const network: rive.Rive = new rive.Rive({
+// init rive
+const network = new rive.Rive({
     src: "./neural_network.riv",
     canvas: document.getElementById('canvas') as HTMLCanvasElement,
 });
+
+// set the canvas to a particular frame in the animation
+const setTime = (time: number) => {
+    // @ts-expect-error
+    let animation = network.animator?.animations[0];
+    if (animation) {
+        animation.instance.time = time;
+        // @ts-expect-error
+        animation.instance.apply(network.artboard, 1.0);
+        // @ts-expect-error
+        network.artboard.advance(0);
+        // @ts-expect-error
+        network.artboard.draw(network.renderer);
+    }
+}
+
+// set up detector for mouse move event
+const detector = document.getElementById("detector");
+let mousePos = 0;
+detector.addEventListener("mousemove", (e) => {
+    mousePos = e.offsetX / detector.clientWidth;
+});
+
+// update canvas according to mouse position
+const updateBoard = () => {
+    setTime(mousePos);
+    window.requestAnimationFrame(updateBoard);
+}
+window.requestAnimationFrame(updateBoard);
